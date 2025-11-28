ec16cc4b1d9689e841f4669e5b729d639330125e
thsalrkd <6222mk@naver.com>
2025-11-28 21:45:05 +0900

    mk
    edit map

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager instance;
+    public Playermove player;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        instance = this;
+
     }
 }
